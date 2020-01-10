e7fa11e8ff4c05593318deb4df6a88645334a63a
kishimoto-banana <drehbleistift@gmail.com>
2020-01-10 23:48:37 +0900

    [WIP]hatebu predict

---
+++ app/lib/predictor.py
@@ -0,0 +1,15 @@
+from scipy.sparse import csr_matrix
+from catboost import CatBoostRegressor
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def predict_hatebu(main_text: str, words: list) -> int:
+
+    # load ectorizer
+    # vectroize
+    # count char and word
+    # load model
+    # predict via model
+
+    pass
---
+++ app/lib/tokenizer.py
@@ -0,0 +1,31 @@
+import re
+import MeCab
+import mojimoji
+
+
+def tokenize(sentence: str, tagger: MeCab.Tagger, stopwords: list, target_parts_of_speech: list) -> list:
+    sentence = re.sub(r'https?://[\w/:%#\$&\?~\.=\+\-]+', '', sentence)
+        
+    tokenized_sentence = []
+    regex = re.compile(r'\d')
+    regax_one = re.compile(r"^\w$")
+    try:
+        for chunk in tagger.parse(sentence).splitlines()[:-1]:
+            (surface, feature) = chunk.split('\t')
+            if feature.startswith(target_parts_of_speech):
+                if surface not in stopwords:
+                    if not regex.search(surface) and not regax_one.search(surface):
+                        word = word_normiraze(surface)
+                        tokenized_sentence.append(word)
+    except Exception:
+        return []
+                    
+    return tokenized_sentence
+
+
+def word_normiraze(word: str) -> str:
+    word = word.lower()
+    word = mojimoji.han_to_zen(word, ascii=False, digit=False)
+    word = mojimoji.zen_to_han(word, kana=False)
+
+    return word
