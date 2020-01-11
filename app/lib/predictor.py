5cc3b5854f796e48cb42173dc3fd802c02fac0f0
kishimoto-banana <drehbleistift@gmail.com>
2020-01-11 22:26:31 +0900

    Add predictor

--- app/lib/predictor.py
+++ app/lib/predictor.py
@@ -1,15 +1,30 @@
-from scipy.sparse import csr_matrix
+import math
+import joblib
+import numpy as np
 from catboost import CatBoostRegressor
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import CountVectorizer
 
 
+vectorizer = joblib.load("/models/vectorizer.pkl")
+svd = joblib.load("/models/svd.pkl")
+regressor = joblib.load('/models/regression.pkl')
+
+
+def vectorize(main_text: str, words: list) -> np.array:
+    X_bow = vectorizer.transform([' '.join(words)])
+    X_svd = svd.transform(X_bow)
+
+    char_nums = len(main_text)
+    word_nums = len(words)
+
+    X = np.hstack([X_svd, np.array(char_nums).reshape(1, -1), np.array(word_nums).reshape(1, -1)])
+    return X
+
+
 def predict_hatebu(main_text: str, words: list) -> int:
+    X = vectorize(main_text, words)
+    pred = regressor.predict(X)
+    hatebu = math.floor(pred + 0.5)
 
-    # load ectorizer
-    # vectroize
-    # count char and word
-    # load model
-    # predict via model
-
-    pass
+    return hatebu
