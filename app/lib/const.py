7eb05bf8818fc86d9e8382e83d440303c22f8346
kishimoto-banana <drehbleistift@gmail.com>
2020-01-12 21:06:44 +0900

    Handling page not found

--- app/lib/const.py
+++ app/lib/const.py
@@ -3,7 +3,7 @@ import re
 url_regex = re.compile(r"https?://[\w/:%#\$&\?~\.=\+\-]+")
 
 
-# For exception
+# exception class
 class InvalidUrlError(Exception):
     """有効なURLでは無かったときに投げるエラー"""
 
@@ -20,8 +20,11 @@ class CannotPredictError(Exception):
     """はてブの予測に失敗したときに投げるエラー"""
 
 
+# error code
 invalid_url_code = 2001
 exception_code = 1000
 cannot_scrape_code = 1001
 cannot_tokenize_code = 1002
 cannot_predict_code = 1003
+
+not_exist_title = "Entry is not found"
--- app/lib/scraper.py
+++ app/lib/scraper.py
@@ -2,15 +2,18 @@ import requests
 import html2text
 from readability.readability import Document
 
-from .const import CannotScrapeError
-
-# Not foundのとき
+from .const import CannotScrapeError, not_exist_title
 
 
 def scrape(url: str) -> str:
     try:
         res = requests.get(url)
         article = Document(res.content).summary()
+        title = Document(res.content).short_title()
+
+        if title == not_exist_title:
+            raise CannotScrapeError
+
         main_text = html2text.html2text(article)
     except Exception:
         raise CannotScrapeError
